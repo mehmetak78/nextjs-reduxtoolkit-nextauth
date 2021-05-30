3b4399a1e2a32580fb3b2746a07d2dbc4b6ad6d0
mehmetak <mehmetak78@hotmail.com>
2021-05-31 01:15:42 +0300

    Home

--- config/dev.js
+++ config/dev.js
@@ -1,4 +1,4 @@
 module.exports = {
-    mongoURI: 'mongodb+srv://mehmet:mustafa@cluster0.h2qfr.mongodb.net/meetups?retryWrites=true&w=majority'
+    mongoURI: 'mongodb+srv://mehmet:****@cluster0.h2qfr.mongodb.net/meetups?retryWrites=true&w=majority'
 };
 
